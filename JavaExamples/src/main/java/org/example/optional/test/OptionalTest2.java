a5329eb35c3a3302c95fac4eba68dc6542bca3b3
Marcio Oliveira <marcio.justo@gmail.com>
2020-02-03 23:46:21 +0000

    Fim de optionals

--- JavaExamples/src/main/java/org/example/optional/test/OptionalTest2.java
+++ JavaExamples/src/main/java/org/example/optional/test/OptionalTest2.java
@@ -11,9 +11,32 @@ public class OptionalTest2 {
     public static void main(String[] args) {
         Seguradora seguradora = new Seguradora("DevDojo Seguros");
         Carro carro = new Carro(seguradora, "Audi");
-        Pessoa pessoa = new Pessoa(carro, "Carlos");
+        Pessoa pessoa = new Pessoa(carro, "Carlos", 20);
         //pessoa = null;
         System.out.println(getNomeSeguradora(Optional.ofNullable(pessoa)));
+        checarNomeSeguradoraOptional(seguradora);
+        System.out.println(obterNomeSeguradoraComIdade(pessoa, 20));
+    }
+
+    private static String obterNomeSeguradoraComIdade(Pessoa p, int idadeMinima) {
+        return Optional.ofNullable(p)
+                .filter(pessoa -> pessoa.getIdade() >= idadeMinima)
+                .flatMap(Pessoa::getCarro)
+                .flatMap(Carro::getSeguradora)
+                .map(Seguradora::getNome)
+                .orElse("Não existe seguradora ou idade mínima não foi atendida");
+    }
+
+    private static void checarNomeSeguradora(Seguradora seguradora) {
+        if(seguradora != null && seguradora.getNome().equalsIgnoreCase("DevDojo Seguros")) {
+            System.out.println("É o DevDojo!");
+        }
+    }
+
+    private static void checarNomeSeguradoraOptional(Seguradora seguradora) {
+        Optional.ofNullable(seguradora)
+                .filter(s -> s.getNome().equalsIgnoreCase("DevDojo Seguros"))
+                .ifPresent(x -> System.out.println("É o DevDojo!"));
     }
 
     public static String getNomeSeguradora(Optional<Pessoa> pessoa) {
