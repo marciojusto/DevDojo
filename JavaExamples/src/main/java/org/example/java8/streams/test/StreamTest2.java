90707c6f63a7ae448e4bd5d150428d2d6331b1b0
Marcio Oliveira <marcio.justo@gmail.com>
2020-02-23 19:18:30 +0000

    streams part 3

---
+++ JavaExamples/src/main/java/org/example/java8/streams/test/StreamTest2.java
@@ -0,0 +1,27 @@
+package org.example.java8.streams.test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.asList;
+
+public class StreamTest2 {
+
+    public static void main(String[] args) {
+
+        List<List<String>> nomes = new ArrayList<>();
+
+        nomes.add(asList("DevDojo", "William"));
+        nomes.add(asList("Marcio", "Justo"));
+
+        System.out.println(nomes);
+
+        List<String> strings = nomes.stream()
+                                    .flatMap(Collection::stream)
+                                    .collect(Collectors.toList());
+
+        System.out.println(strings);
+    }
+}
