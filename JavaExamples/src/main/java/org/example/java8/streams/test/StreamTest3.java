7b29e86c20af756adb00c5c88a8d21d2449e9d03
Marcio Oliveira <marcio.justo@gmail.com>
2020-02-29 12:09:43 +0000

    streams part 2 and 3

---
+++ JavaExamples/src/main/java/org/example/java8/streams/test/StreamTest3.java
@@ -0,0 +1,27 @@
+package org.example.java8.streams.test;
+
+import org.example.java8.streams.classes.Pessoa;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class StreamTest3 {
+
+    public static void main(String[] args) {
+        List<Pessoa> pessoas = Pessoa.bancoDePessoas();
+        System.out.println(pessoas.stream().anyMatch(pessoa -> pessoa.getSalario() > 4000));
+        System.out.println(pessoas.stream().allMatch(pessoa -> pessoa.getIdade() > 18));
+        System.out.println(pessoas.stream().noneMatch(pessoa -> pessoa.getIdade() < 18));
+
+        pessoas.stream()
+               .filter(pessoa -> pessoa.getIdade() < 25)
+               .findAny()
+               .ifPresent(System.out::println);
+
+        pessoas.stream()
+               .filter(pessoa -> pessoa.getIdade() > 25)
+               .sorted(Comparator.comparing(Pessoa::getIdade).reversed())
+               .findFirst()
+               .ifPresent(System.out::println);
+    }
+}
